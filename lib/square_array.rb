a262f5584811241792d0d4e27cdb7072510241a6
Marc Blanchard <marcblanchard0930@gmail.com>
2019-07-20 01:36:42 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -5,7 +5,7 @@ def square_array(array)
   while count <= array.length do
     squared_arrays = array[count]
     square = squared_arrays ** 2
-    array.push(square)
+    array.push(square[count])
     count += 1
   end
   return square_array(square)
